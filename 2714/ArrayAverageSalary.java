b26a14e2359cdadf9943c84661f24ac5f921ee38
Shan <36489952+shanmugaa16@users.noreply.github.com>
2018-02-22 22:43:58 +0000

    Update ArrayAverageSalary.java
     Solution:
     Salary of employee 1: 30000
     Salary of employee 2: 35000
     Salary of employee 3: 36000
     Salary of employee 4: 45000
     Salary of employee 5: 50000
    Average salary of 5 employees are: 39200.0

--- 2714/ArrayAverageSalary.java
+++ 2714/ArrayAverageSalary.java
@@ -9,17 +9,21 @@ public class ArrayAverageSalary {
 
 	public static void main(String[] args) {
 		
-		int salary[] = new int[] {30000,35000,36000,45000,50000};                   //Define an array and assign values for salary
+		int salary[] = new int[] {30000,35000,36000,45000,50000};                       //Define an array and assign values for salary
 		int sumSalary = 0;
 		
 		for (int i=0;i<salary.length;i++) {   
 			System.out.println(" Salary of employee " +(i+1)+ ": " +salary[i]);     //Display salary of each employee 
-			sumSalary = sumSalary + salary[i];                        				//Calculate Sum of salary
+			sumSalary = sumSalary + salary[i];                                      //Calculate Sum of salary
 		}
 		
-		double avgSalary = sumSalary/salary.length;									//Calculate Average Salary
-		System.out.println("Average salary of 5 employees are: "+avgSalary);        //Display Average Salary to output console
+		double avgSalary = sumSalary/salary.length;				        //Calculate Average Salary
+		System.out.println("Average salary of 5 employees are: "+avgSalary);            //Display Average Salary to output console
 
 	}
 
 }
+
+
+	
+	
