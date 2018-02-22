60d5112da1e02030535efd3a4292335f64b0a07a
Shan <36489952+shanmugaa16@users.noreply.github.com>
2018-02-22 17:02:06 -0600

    Add files via upload

---
+++ 2714/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.corejava.exercise;
+
+/*  Creating first java application program */
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! - My First program");
+
+	}
+
+}
