b38f4cdd82070818abe1105a0bf51a09e2a6aa70
Shan <36489952+shanmugaa16@users.noreply.github.com>
2018-02-22 23:06:52 +0000

    EvenNumbers.Java

--- 2714/EvenNumbers.java
+++ 2714/EvenNumbers.java
@@ -7,14 +7,18 @@ public class EvenNumbers {
 	public static void main(String[] args) {
 		
 		int num = 100;
-		System.out.println(" Even numbers within 100 are:");
+		System.out.println("Even numbers within 100 are:");        //Print Even numbers
 		
 		for (int i =1;i<=num;i++) {
 			if(i%2 == 0) {
-				System.out.print(i+"\n");
+				System.out.print(i+" ");
 			}
 		}
 
 	}
 
 }
+
+Solution:
+Even numbers within 100 are:
+2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92 94 96 98 100 
