cd31342e2b5d9b532e85adf198650b6885fd7cba
Narendranath Krishnamurthy <naren.krishna31@gmail.com>
2018-02-22 22:15:46 -0600

    WorldPopulation exercise

---
+++ 1541/JavaBeginers/src/com/htc/basic/javabasic/WorldPopulation.java
@@ -0,0 +1,14 @@
+package com.htc.basic.javabasic;
+
+public class WorldPopulation {
+
+	public static void main(String[] args) {
+		
+		long population = 6000000000L; 
+		for(int year= 2001 ; year < 2010; year ++){
+			population = (long) (population + (population * 0.01));
+			System.out.println("Population for " + year + " is " + population);
+		}
+	}
+
+}
