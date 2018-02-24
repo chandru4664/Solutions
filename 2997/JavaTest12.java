2ad1bed218b7b73f6d212b5af2ccab3afac6873f
chandraganesh1991 <36512268+chandraganesh1991@users.noreply.github.com>
2018-02-24 08:15:57 -0600

    Add files via upload

---
+++ 2997/JavaTest12.java
@@ -0,0 +1,12 @@
+package com.htc.core.java.day1;
+
+public class JavaTest12 {
+
+	public static void main(String[] args) {
+		byte a=10;
+		byte b=20;
+		byte c= (byte) (a*b);
+		System.out.println(c);
+	}
+
+}
---
+++ 2997/JavaTest5.java
@@ -0,0 +1,17 @@
+package com.htc.core.java.day1;
+
+public class JavaTest5 {
+
+	public static void main(String[] args) {
+		char letters = '\u0041';
+		System.out.println("Alphabets from A to Z");
+
+		while (letters<= '\u005A')
+		{
+			System.out.println(letters);
+			letters++;
+		}
+
+	}
+
+}
