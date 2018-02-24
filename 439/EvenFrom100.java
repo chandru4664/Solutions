051b42c746ec78ea683f0ce52c7a4cc2aa1bba12
prasannamate <prasannamate@yahoo.com>
2018-02-24 16:42:50 -0600

    Add files via upload

---
+++ 439/EvenFrom100.java
@@ -0,0 +1,15 @@
+package com.htc.corejava.day1;
+
+public class EvenFrom100 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Print all even numbers from 0 to 100.
+		// Written by Prasanna Mate
+		// 22-Feb-2018.
+		for (int i = 2 ; i <= 100; i=i+2) {
+			System.out.println(i);
+		}
+	}
+ 
+}
