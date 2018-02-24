77cfa97fcfc176060ef9e7e749dacf1fb0a5073d
prasannamate <prasannamate@yahoo.com>
2018-02-24 16:42:10 -0600

    Add files via upload

---
+++ 439/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.htc.corejava.day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Created by Prasanna Mate
+		// 21-Feb-2018.
+		System.out.println("Welocme to Java");
+	}
+
+}
