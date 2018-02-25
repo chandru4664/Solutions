232a44380771342b0561d587f64bea78d34c888e
harimenonpk <31015275+harimenonpk@users.noreply.github.com>
2018-02-25 00:43:23 -0600

    Add files via upload

---
+++ 1251/DataTypes.java
@@ -0,0 +1,29 @@
+package com.htc.coreexercise.beginers;
+
+public class DataTypes {
+
+	public static void main(String[] args) {//declare dataTypes and print them out
+			int area= 2500;
+			byte number=12;
+			short parking=1;
+			long location=1245566778L;
+			String name="Mahabalipuram";
+			char postOffice='E';
+			boolean sold=false;
+			double cost=298398323;
+			float height=14.66f;
+			
+			System.out.println("Area-->"+ area);
+			System.out.println("Number-->" + number);
+			System.out.println("Parking-->"+ parking);
+			System.out.println("Location-->" + location);
+			System.out.println("Name-->" + name);
+			System.out.println("Post Office-->"+ postOffice);
+			System.out.println("Sold? -->" + sold);
+			System.out.println("Cost-->" + cost);
+			System.out.println("height-->" + height);
+	
+	}
+
+}
+
