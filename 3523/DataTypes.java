a18035e790b0cf2cd35890e761b0c10a89e2afef
jishnu25 <34361598+jishnu25@users.noreply.github.com>
2018-03-06 19:25:21 -0600

    Add files via upload

---
+++ 3523/DataTypes.java
@@ -0,0 +1,27 @@
+package com.htc.hadoop.classwork;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+			byte b =100;
+			short s =123;
+			int v = 123543;
+			int calc = -9876345;
+			long amountVal = 1234567891;
+			float intrestRate = 12.25f;
+			double sineVal = 12345.234d;
+			boolean flag = true;
+			boolean val = false;
+			System.out.println("byte Value = "+ b);
+			System.out.println("short Value = "+ s);
+			System.out.println("int Value = "+ v);
+			System.out.println("int second Value = "+ calc);
+			System.out.println("long Value = "+ amountVal);
+			System.out.println("float Value = "+ intrestRate);
+			System.out.println("double Value = "+ sineVal);
+			System.out.println("boolean Value = "+ flag);
+			System.out.println("boolean Value = "+ val);
+			
+		}
+	
+}
